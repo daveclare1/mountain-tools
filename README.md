e66ebdaa2d158d9d10a0c23b726a5989e9e260c9
David Clare <davidclare35@gmail.com>
2023-02-19 18:35:20 +0000

    Updated app, separated prep from map drawing

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # mountain-tools
+
+This is a tool to colour Scottish mountain slopes in the standard avalanche forecast colours.
+It doesn't actively link to any forecasts. It uses the SAIS website to draw a pretty forecast diagram, but only from the data you input!
+
+The elevation dataset used was downlaoded from Copernicus, and is eu_dem_v11_E30N30. It was then trimmed down to the most mountainous areas.
+QGIS 3.28.2 was used to process this into avalanche zones, and .model3 files are provided with a few approaches.
+sais_categorisation_3.model3 runs the fastest but on the whole of scotland dataset sometimes trips up, in which case manual processing can be done:
+- generate elevation polygons in increments of 100m
+- generate slope and aspect rasters
+- create slope polygons with increments of 25 degrees and discard all but the 25-50 deg zones
+- expand these polygons and use them to trim the aspect raster
+- create aspect polygons in increments of 45 deg, starting at 22.5 deg
+- intersect the aspect and slope polygons
+- fix the geometry and remove any areas < 2500m2
+- intersect with the elevation polygons
+- fix the geometry and remove any areas < 2500m2
+- simplify the geometry to the nearest 25m
+- convert to the 4326 coordinate system while exporting a .gpkg file
--- streamlit-app.py
+++ streamlit-app.py
@@ -3,7 +3,7 @@ import plotly.express as px
 import geopandas as gpd
 import json
 
-FILENAME = "cairngorms_avi_zones_4326.gpkg"
+FILENAME = "scotland_avi_zones_4326_simple.gpkg"
 
 forecast_colors = {
     "0": 'green',
@@ -12,6 +12,30 @@ forecast_colors = {
     "3": 'red',
     "4": 'black'
 }
+
+# the map polygons have an aspect_min attribute, this is how they map to the index of the colour string
+map_aspect_min_to_category = {
+    0: 0,
+    22.5: 1,
+    67.5: 2,
+    112.5: 3,
+    157.5: 4,
+    202.5: 5,
+    247.5: 6,
+    292.5: 7,
+    337.5: 0,
+}
+
+aspect_names = [
+    'N',
+    'NW',
+    'E',
+    'SE',
+    'S',
+    'SW',
+    'W',
+    'NW'
+    ]
 
 def forecast_string_to_url(fl, fh, sm, sb):
     val = ''.join([f"{int(l)+1}{int(h)+1}00" for l, h in zip(fl, fh)])
@@ -22,6 +46,10 @@ def load_polygons():
     fp = FILENAME
     map_df = gpd.read_file(fp).set_crs("EPSG:4326", allow_override=True)
     map_df['poly_id'] = map_df.index    # Create column of unique ids
+    map_df['aspect_category'] = map_df.apply(lambda row: map_aspect_min_to_category[row.ASPECT_MIN], axis=1)
+    map_df['aspect'] = map_df.apply(lambda row: aspect_names[row.aspect_category], axis=1)
+    map_df['elevation'] = map_df.apply(lambda row: f"{row.ELEV_MIN:.0f}-{row.ELEV_MAX:.0f}m", axis=1)
+    print(map_df.describe())
     return map_df
 
 def assign_forecast(df_row, fl, fh, sm, sb):
@@ -29,17 +57,19 @@ def assign_forecast(df_row, fl, fh, sm, sb):
         return '0'
 
     if df_row.ELEV_MIN < sm:
-        return fl[df_row.aspect-1]
+        return fl[df_row.aspect_category]
     else:
-        return fh[df_row.aspect-1]
+        return fh[df_row.aspect_category]
 
 st.set_page_config(layout="wide")
-st.title("SAIS Painting Tool")
+st.title("Avalanche Painting Tool")
 st.markdown(
     """
-    Use the controls on the left to define the avalanche map you want.
-    All areas on the map will be coloured accordingly. Numbers 0-4 represent green to black,
-    counted around from N to NW. 
+    Use the controls on the left to define the avalanche chart you want.
+    All areas on the map will be coloured accordingly. Numbers 0-4 represent green, yellow, orange, red and black,
+    counted around clockwise from N to NW. Make sure to set the elevation values correctly too.
+
+    Once the chart looks right, hit the button to generate the map (takes some time, and transfers a lot of data)
     """)
 
 elev_split = st.sidebar.number_input("Elevation Split", value=700, step=100)
@@ -51,32 +81,37 @@ st.sidebar.image(forecast_string_to_url(forecast_low, forecast_high, elev_split,
 
 map_df = load_polygons()
 
-map_df['forecast'] = map_df.apply(assign_forecast, 
-                                args=(forecast_low, forecast_high, elev_split, elev_bottom),
-                                axis=1)
+if st.button('Click to draw map'):
+    with st.spinner('Painting map...'):
+        map_df['forecast'] = map_df.apply(assign_forecast, 
+                                        args=(forecast_low, forecast_high, elev_split, elev_bottom),
+                                        axis=1)
 
-fig = px.choropleth_mapbox(map_df, geojson=map_df.geometry, 
-                    locations="poly_id",
-                    color="forecast",
-                    color_discrete_map=forecast_colors,
-                    mapbox_style="stamen-terrain",
-                    zoom=8, center = {"lat": 57.123, "lon": -3.670},
-                    opacity=0.5,
-                    hover_data={"aspect":True, "forecast":False, "ELEV_MIN":True, "poly_id":False}
-                    )
+        fig = px.choropleth_mapbox(map_df, geojson=map_df.geometry, 
+                            locations="poly_id",
+                            color="forecast",
+                            color_discrete_map=forecast_colors,
+                            mapbox_style="stamen-terrain",
+                            zoom=8, center = {"lat": 57.123, "lon": -3.670},
+                            opacity=0.5,
+                            hover_data={"aspect":True, "forecast":False, "elevation":True, "poly_id":False}
+                            )
 
-fig.update_geos(fitbounds="geojson", visible=True)
-fig.update_traces(marker_line_width=0)
-fig.update_layout(showlegend=False, height=600)
+        fig.update_geos(fitbounds="geojson", visible=True)
+        fig.update_traces(marker_line_width=0)
+        fig.update_layout(showlegend=False, height=600)
 
-st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig, use_container_width=True)
 
 st.markdown(
     """
     Important info:
     - Elevation data is the EU-DEM v1.1 dataset from [Copernicus](https://land.copernicus.eu/imagery-in-situ/eu-dem/eu-dem-v1.1)
+    - Only areas vaguely in and around the SAIS forecast areas are drawn over, the rest of Scotland is ignored
     - The coloured slopes are angled 25-50 deg
-    - The resolution of the dataset is low (1 point per 25m). The slope calculation smooths this out too. Slope boundaries are loose!
+    - The resolution of the base dataset is low (1 point per 25m). The slope calculation smooths this out too. Slope divisions are therefore quite loose
+    - Areas that fit all other criteria but are smaller than 50x50m are not included
+    - Even after all of this, the shapes are further simplified to give a reasonable filesize
     - For goodness sake use common sense
     """
 )
